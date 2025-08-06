4774d44a06690cbe78ac3e8808a7984cbf3c339e
Javier David Carnelli <javier.carnelli@conductorone.com>
2025-08-06 14:24:21 -0300

    test: config validation removed cause there is nothing to validate

--- pkg/config/config_test.go
+++ pkg/config/config_test.go
@@ -1,45 +1,10 @@
 package config
 
 import (
-	"os"
 	"testing"
-
-	"github.com/conductorone/baton-sdk/pkg/field"
-	"github.com/stretchr/testify/assert"
 )
 
-var accessToken = os.Getenv("OUTREACH_ACCESS_TOKEN")
-
 func TestValidateConfig(t *testing.T) {
-	tests := []struct {
-		name    string
-		config  *Outreach
-		wantErr bool
-	}{
-		{
-			name: "valid config",
-			config: &Outreach{
-				AccessToken: accessToken,
-			},
-			wantErr: false,
-		},
-		{
-			name: "invalid config - missing required fields",
-			config: &Outreach{
-				AccessToken: "",
-			},
-			wantErr: true,
-		},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			err := field.Validate(Config, tt.config)
-			if tt.wantErr {
-				assert.Error(t, err)
-			} else {
-				assert.NoError(t, err)
-			}
-		})
-	}
+	// There is no point on validating config since it's an OAuth connector.
+	// Providing and access token is optional for a CLI execution and doing tests.
 }
